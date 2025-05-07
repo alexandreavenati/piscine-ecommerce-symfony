76563bcd1aefba424bce0de02af7036110d63a16
Ronnawit Raksachob <alexandre.avenati@lapiscine.pro>
2025-05-07 16:32:46 +0200

    Ajout de css pour les produits et les catégories + ajout de liaison pour les catégories et les produits

--- src/Controller/guest/ProductController.php
+++ src/Controller/guest/ProductController.php
@@ -11,20 +11,22 @@ use App\Repository\ProductRepository;
 class ProductController extends AbstractController {
 
     #[Route('/produits', name: 'product-list')]
-    public function displayListCategory(ProductRepository $productRepository)
+    public function displayListCategory(ProductRepository $productRepository, CategoryRepository $categoryRepository)
     {
 
         $products = $productRepository->findAll();
+        $category = $categoryRepository->findAll();
 
-        return $this->render('guest/products-list.html.twig', ['products' => $products]);
+        return $this->render('guest/products-list.html.twig', ['products' => $products, 'categories' => $category]);
     }
 
     #[Route('/produit/{id}', name: 'show-product')]
-    public function showCategory(ProductRepository $productRepository, $id)
+    public function showCategory(ProductRepository $productRepository, CategoryRepository $categoryRepository, $id)
     {
 
         $product = $productRepository->find($id);
+        $category = $categoryRepository->findAll();
 
-        return $this->render('guest/show-product.html.twig', ['product' => $product]);
+        return $this->render('guest/show-product.html.twig', ['product' => $product, 'category' => $category]);
     }
 }
