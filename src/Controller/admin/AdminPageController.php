0a474a764b858a47d16829573566cbe165fc8685
Ronnawit Raksachob <alexandre.avenati@lapiscine.pro>
2025-05-12 15:47:59 +0200

    Ajout dde bouton pour se connecter, redirection automatique sur admin-home après connection et ajout de bouton déconnection

--- src/Controller/admin/AdminPageController.php
+++ src/Controller/admin/AdminPageController.php
@@ -5,11 +5,13 @@ namespace App\Controller\admin;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class AdminPageController extends AbstractController
 {
 
     #[Route('/admin', name: 'home-admin')]
+    #[IsGranted('ROLE_ADMIN')]
     public function displayHome()
     {
         return $this->render('admin/admin-home.html.twig');
---
+++ src/Controller/guest/LoginController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller\guest;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class LoginController extends AbstractController
+{
+
+    #[Route('/login', name: 'login')]
+    public function displayLogin(AuthenticationUtils $authenticationUtils)
+    {
+
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        return $this->render('guest/login.html.twig', ['error' => $error]);
+    }
+
+    #[Route('/redirect-after-login', name: 'redirect_after_login')]
+    public function redirectAfterLogin(): Response
+    {
+        $user = $this->getUser();
+
+        if ($this->isGranted('ROLE_ADMIN')) {
+            return $this->redirectToRoute('home-admin');
+        }
+
+        // Redirection par défaut
+        return $this->redirectToRoute('home');
+    }
+
+    #[Route('/logout', name:'logout')]
+    public function logout() {
+        
+    }
+}
