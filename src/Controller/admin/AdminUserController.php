8bce1326e6cf8dd8cc7b12c31a5d6ff28a4dbf40
Ronnawit Raksachob <alexandre.avenati@lapiscine.pro>
2025-05-13 10:36:40 +0200

    Ajout d'une page liste d'admins et redirection vers cette page après création de compte admin

--- src/Controller/admin/AdminUserController.php
+++ src/Controller/admin/AdminUserController.php
@@ -3,18 +3,23 @@
 namespace App\Controller\admin;
 
 use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
 use Doctrine\ORM\EntityManagerInterface;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
-class AdminUserController extends AbstractController {
+class AdminUserController extends AbstractController
+{
 
-    #[Route('/admin/create-user', name:'admin-create-user')]
-    public function displayCreateUser(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager){
+    #[Route('/admin/create-user', name: 'admin-create-user')]
+    public function displayCreateUser(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager)
+    {
 
-        if($request->isMethod('POST')){
+        if ($request->isMethod('POST')) {
 
             $email = $request->request->get('email');
             $password = $request->request->get('password');
@@ -25,12 +30,29 @@ class AdminUserController extends AbstractController {
 
             $user->createAdmin($email, $passwordHashed);
 
-            $entityManager->persist($user);
-            $entityManager->flush();
+            try {
+                $entityManager->persist($user);
+                $entityManager->flush();
 
-            $this->addFlash("admin_created", "Le compte administrateur a été créé.");
+                $this->addFlash("admin_created", "Le compte administrateur a été créé.");
+                return $this->redirectToRoute('admin-list-admin');
+            } catch (UniqueConstraintViolationException $e) {
+
+                $this->addFlash("error_email_taken", "Erreur : email déjà pris.");
+            } catch (Exception $e) {
+
+                $this->addFlash("error_admin_created", "Erreur : Impossible de créer l'admin.");
+            }
         }
 
         return $this->render('/admin/user/create-user.html.twig');
+    }
+
+    #[Route('/admin/list-admin', name:'admin-list-admin')]
+    public function displayListAdmins(UserRepository $userRepository) {
+
+        $users = $userRepository->findAll();
+
+		return $this->render('/admin/user/admin-list.html.twig', ['users' => $users]);
     }
 }
